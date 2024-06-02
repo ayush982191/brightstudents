3e71bc549857569d4ed94989c43a70a825a655b0
Ayush <ayushkumar982191@gmail.com>
2024-06-02 11:34:22 +0530

    add

--- src/components/BlueAlphabetCard.tsx
+++ src/components/BlueAlphabetCard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import ReactCardFlip from "react-card-flip";
 import heartcard from "../assets/blueHeartCard.png";
 
